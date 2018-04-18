64c8b72caf3ded806b9977948c99827825589262
tomzhangqc <30365746+tomzhangqc@users.noreply.github.com>
2018-04-18 15:54:17 +0800

    springboot+dubbo

--- provider-parent/service/src/main/java/com/forum/service/impl/DubboProviderServiceImpl.java
+++ provider-parent/service/src/main/java/com/forum/service/impl/DubboProviderServiceImpl.java
@@ -3,11 +3,11 @@ package com.forum.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.forum.service.DubboProviderService;
 
-@Service
+@Service(version = "1.0.0")
 public class DubboProviderServiceImpl implements DubboProviderService {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	@Override
